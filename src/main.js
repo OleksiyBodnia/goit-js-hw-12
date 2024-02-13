0b6d24615cb1bb584fd79dec4b875502427d957c
OleksiyBodnia <90392804+OleksiyBodnia@users.noreply.github.com>
2024-02-13 23:31:58 +0200

    add solution for home work

--- src/main.js
+++ src/main.js
@@ -2,8 +2,81 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import axios from 'axios';
 
-const listOfPhotos = document.querySelector('.photos');
+
+const listOfPhotos = document.querySelector('.container ul');
+const loader = document.querySelector('.loader');
+const form = document.querySelector('.form');
+const input = document.querySelector('input');
+
+const loadMoreBtn = document.createElement('button');
+loadMoreBtn.textContent = "Load more"
+loadMoreBtn.style.margin = "0 auto";
+loadMoreBtn.style.marginTop = "15px";
+loadMoreBtn.style.width = "150px";
+listOfPhotos.after(loadMoreBtn);
+loadMoreBtn.style.display = 'none';
+
+let inputValue = '';
+let page = 1;
+const perPage = 15;
+const totalPages = Math.ceil(100/perPage);
+
+const searchDefaultParam = {
+    key: '42291336-b4c9ef387c9d7e209159058e7',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+}
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    listOfPhotos.innerHTML = '';
+    inputValue = input.value.trim();
+
+    if (!inputValue){
+        iziToast.error({
+            title: 'Error',
+            message: 'Please enter a search term',
+            position: 'topRight'
+        });
+    } else {
+        loader.style.display = 'block';
+        loadMoreBtn.style.display = 'none';
+
+        try {
+            const response = await axios.get(`?`, {
+                params: {
+                    q: inputValue,
+                    ...searchDefaultParam,
+                    page: page,
+                    per_page: perPage
+                }
+            })
+            if (!response.data.hits || response.data.hits.length === 0) {
+                iziToast.error({
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    position: "topRight",
+                    icon: ""
+                });
+            } else {
+                renderingPhotos(response.data.hits);
+                loadMoreBtn.style.display = 'block';
+            }
+
+            loader.style.display = 'none';
+        } catch (error) {
+            loader.style.display = 'none';
+            loadMoreBtn.style.display = 'none';
+            console.log(error);
+        }
+    }
+
+    form.reset();
+});
+
 const renderingPhotos = data => {
     data.forEach(element => {
 
@@ -28,50 +101,39 @@ const renderingPhotos = data => {
     lightbox.refresh();
 }
 
-const loader = document.querySelector('.loader');
-const form = document.querySelector('.form');
-const input = document.querySelector('input');
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const inputValue = input.value;
-
-    if (!inputValue.trim()){
-        iziToast.error({
-            title: 'Error',
-            message: 'Please enter a search term',
-            position: 'topRight'
+loadMoreBtn.addEventListener('click', async (event) => {
+    loader.style.display = 'block';
+    if (page > totalPages) {
+        loader.style.display = 'none';
+        loadMoreBtn.style.display = 'none';
+        return iziToast.error({
+          position: "topRight",
+          message: "We're sorry, there are no more posts to load"
         });
-    } else {
-        loader.style.display = 'block';
+      }
+
+    try {
+        page++;
+        const response = await axios.get(`?`, {
+            params: {
+                q: inputValue,
+                ...searchDefaultParam,
+                page: page,
+                per_page: perPage
+            }
+        })        
+        loader.style.display = 'none';
+        renderingPhotos(response.data.hits);
+        const cardHeight = listOfPhotos.firstElementChild.getBoundingClientRect().height;
         
-        fetch(`https://pixabay.com/api/?key=42291336-b4c9ef387c9d7e209159058e7&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(response.status);
-                }
-                return response.json();
-            })
-            .then(data => {
-                loader.style.display = 'none';
+        window.scrollBy({
+            top: cardHeight * 3.45, 
+            behavior: 'smooth'
+        });
+    } catch (error){
+        loader.style.display = 'none';
+        console.log(error);
+    }
+})
 
-                if (data.hits.length === 0){
-                    iziToast.error({
-                        message: 'Sorry, there are no images matching your search query. Please try again!',
-                        position: "topRight",
-                        icon: ""
-                    });
-                } else {
-                    renderingPhotos(data.hits);
-                }
-            })
-            .catch(error => {
-                loader.style.display = 'none';
-                console.log(error)
-            });
-        }
 
-        form.reset();
-        listOfPhotos.innerHTML = ''
-});
