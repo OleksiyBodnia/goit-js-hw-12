5e3586c98b9f97b834ed41cf24272452015765fb
OleksiyBodnia <90392804+OleksiyBodnia@users.noreply.github.com>
2024-02-15 00:29:25 +0200

    more updates

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import { searchPixabay } from './js/pixabay-api.js';
+import { renderPhotos } from './js/render-functions.js';
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-import axios from 'axios';
-
 
 const listOfPhotos = document.querySelector('.container ul');
 const loader = document.querySelector('.loader');
@@ -11,7 +9,7 @@ const form = document.querySelector('.form');
 const input = document.querySelector('input');
 
 const loadMoreBtn = document.createElement('button');
-loadMoreBtn.textContent = "Load more"
+loadMoreBtn.textContent = "Load more";
 loadMoreBtn.style.margin = "0 auto";
 loadMoreBtn.style.marginTop = "15px";
 loadMoreBtn.style.width = "150px";
@@ -21,34 +19,7 @@ loadMoreBtn.style.display = 'none';
 let inputValue = '';
 let page = 1;
 const perPage = 15;
-
-const searchDefaultParam = {
-    key: '42291336-b4c9ef387c9d7e209159058e7',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-}
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-
-const getTotalHits = async () => {
-    try{
-        const response = await axios.get('?', {
-            params: {
-                q: inputValue,
-                ...searchDefaultParam,
-                page: 1,
-                per_page: perPage,
-            }
-        });
-        return response.data.totalHits;
-    } catch (error){
-        console.log(error);
-        return 0;
-    }
-}
-
-const totalHits = getTotalHits();
-let totalPages = Math.ceil(totalHits/perPage);
+let totalPages = 1;
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
@@ -67,29 +38,31 @@ form.addEventListener('submit', async (event) => {
         loadMoreBtn.style.display = 'none';
 
         try {
-            const response = await axios.get(`?`, {
-                params: {
-                    q: inputValue,
-                    ...searchDefaultParam,
-                    page: page,
-                    per_page: perPage
-                }
-            })
-            if (!response.data.hits || response.data.hits.length === 0) {
+            const response = await searchPixabay(inputValue, page, perPage);
+
+            const totalHits = response.totalHits;
+            totalPages = Math.ceil(totalHits / perPage);
+
+            if (totalHits === 0) {
                 iziToast.error({
                     message: 'Sorry, there are no images matching your search query. Please try again!',
                     position: "topRight",
                     icon: ""
                 });
             } else {
-                renderingPhotos(response.data.hits);
-                loadMoreBtn.style.display = 'block';
+                renderPhotos(response.hits, listOfPhotos);
+
+                if (response.hits.length < perPage) {
+                    loadMoreBtn.style.display = 'none';
+                } else {
+                    loadMoreBtn.style.display = 'block';
+                }
             }
 
             loader.style.display = 'none';
+
         } catch (error) {
             loader.style.display = 'none';
-            loadMoreBtn.style.display = 'none';
             console.log(error);
         }
     }
@@ -97,45 +70,14 @@ form.addEventListener('submit', async (event) => {
     form.reset();
 });
 
-const renderingPhotos = data => {
-    data.forEach(element => {
-
-        const murkup = `<li class="photo">
-            <a href="${element.largeImageURL}" data-lightbox="photos">
-                <img src="${element.webformatURL}" alt="${element.tags}" title="${element.tags}" class="img">
-            </a>
-            <ul class="list">
-                <li class="item"><h2>Likes <span>${element.likes}</span></h2></li>
-                <li class="item"><h2>Views <span>${element.views}</span></h2></li>
-                <li class="item"><h2>Comments <span>${element.comments}</span></h2></li>
-                <li class="item"><h2>Downloads <span>${element.downloads}</span></h2></li>
-            </ul>
-        </li>`;
-
-        listOfPhotos.insertAdjacentHTML('beforeend', murkup);
-
-    });
-
-    const lightbox = new SimpleLightbox('.photos a', {});
-
-    lightbox.refresh();
-}
-
-loadMoreBtn.addEventListener('click', async (event) => {
+loadMoreBtn.addEventListener('click', async () => {
     loader.style.display = 'block';
 
     try {
         page++;
-        const response = await axios.get(`?`, {
-            params: {
-                q: inputValue,
-                ...searchDefaultParam,
-                page: page,
-                per_page: perPage
-            }
-        });
+        const response = await searchPixabay(inputValue, page, perPage);
         loader.style.display = 'none';
-        renderingPhotos(response.data.hits);
+        renderPhotos(response.hits, listOfPhotos);
         const cardHeight = listOfPhotos.firstElementChild.getBoundingClientRect().height;
         
         window.scrollBy({
@@ -143,7 +85,7 @@ loadMoreBtn.addEventListener('click', async (event) => {
             behavior: 'smooth'
         });
 
-        const newTotalHits = response.data.totalHits;
+        const newTotalHits = response.totalHits;
         totalPages = Math.ceil(newTotalHits / perPage);
 
         if (page >= totalPages) {
@@ -158,7 +100,3 @@ loadMoreBtn.addEventListener('click', async (event) => {
         console.log(error);
     }
 });
-
-
-
-
